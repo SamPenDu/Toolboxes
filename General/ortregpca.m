5f82c99ba63ca04c21858a9cf416df07128e3c06
Sam Schwarzkopf <55238044+SamPenDu@users.noreply.github.com>
2023-05-30 09:17:49 +1200

    Update ortregpca.m

--- General/ortregpca.m
+++ General/ortregpca.m
@@ -9,14 +9,10 @@ if size(Y,1) == 1
     Y = Y';
 end
 
-% Standardise input data
-zX = zscore(X);
-zY = zscore(Y);
-
 % Principal component analysis
-[C,tXY] = pca([zX zY]);
+[C,tXY] = pca([X Y]);
 % Determine variance explained
-Rsq = 1 - sum(tXY(:,2).^2) / sum(zY.^2);
+Rsq = 1 - sum(tXY(:,2).^2) / sum(Y.^2);
 % Orthogonal regression function
 F = (C(1,1)*std(Y)) / (C(2,1)*std(X));
 F = [F, F * -mean(X) + mean(Y)];
