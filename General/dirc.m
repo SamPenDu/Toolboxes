9a0d58e58b923e161d3d0c4ba7fd660bd91bddfc
Sam Schwarzkopf <55238044+SamPenDu@users.noreply.github.com>
2024-09-11 15:28:56 +1200

    Update dirc.m

--- General/dirc.m
+++ General/dirc.m
@@ -2,7 +2,7 @@ function Fs = dirc(dirstr)
 %
 % Fs = dirc(dirstr)
 %
-% Wrapper for dir but returning a cell array of the file nmaes.
+% Wrapper for dir but returning a cell array of the file names.
 %
 
 Fs = dir(dirstr);
